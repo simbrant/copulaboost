8d2b27d775a3e2a898377046854b39e70e8223af
Simon Brant <simonbb@artemis.uio.no>
2020-11-26 18:18:21 +0100

    added ridge p-value stuff

--- R/copulaboost.R
+++ R/copulaboost.R
@@ -3,8 +3,14 @@
 copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
                         learning_rate = .1, eps=5e-2, extra_x=NULL,
                         verbose=F, cont_method="Ingrid_mod", subsample_rows = F,
-                        prop_sample=.1, n_sample = NULL, ml_update=F) {
+                        prop_sample=.1, n_sample = NULL, ml_update=F,
+                        sel_method="p-value") {
 
+  if (sel_method == "p-value-ridge"){
+    invmat <- .comp_invmat_ridge(x, cov_types)
+  } else {
+    invmat <- NULL
+  }
 
   # Make an empty version of the object that will be returned
   model <- lapply(1:(n_models + 1), function(i) vector("list", 2))
@@ -17,29 +23,11 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
 
   model[[1]][[1]] <- intercept
   model[[1]][[2]] <- NULL
-
   current_prediction <- rep(intercept, length(y))
-  scaling <- rep(1, n_models)
+  scaling <- rep(1, n_models + 1)
 
-  # This piece of code compiles the p-values of the regression coefficients
-  # of a univariate linear fit, for each continuous and binary variable,
-  # and the minimum p-value of the regression coefficients of categorical
-  # variables.
-  compute_p_value_weights <- function(y, x, x_type){
-    x <- as.data.frame(x)
-    vals <- rep(1, ncol(x))
-    vals[x_type == "d"] <- apply(x[, x_type=="d"], 2,
-                                 function(x) length(unique(x)) - 1)
-    colind <- cumsum(vals)
-    x[, which(x_type=="d")] <- apply(x[, which(x_type == "d")], 2, as.factor)
-    x[, which(x_type=="c")] <- apply(x[, which(x_type == "c")], 2, as.numeric)
-
-    x_ <- model.matrix(~., data=as.data.frame(x))
-    lmod <- lm(y~x_-1)
-    t_vals <- summary(lmod)$coef[-1, 4]
-    c(min(t_vals[1:colind[1]]),
-      sapply(2:length(colind),
-             function(j) min(t_vals[(colind[j-1] + 1):(colind[j])])))
+  if (verbose) {
+    pb <- txtProgressBar(min = 0, max = n_models + 1, style = 3, initial = 1)
   }
 
   ll <- function(eta, y){
@@ -47,8 +35,8 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
   }
 
   # Compute the weights, and draw the covariates to include.
-  p_val <- compute_p_value_weights(y - plogis(current_prediction), x, cov_types)
-  model[[2]][[2]] <- sample(dim(x)[2], n_covs, prob = 1 - p_val)
+  model[[2]][[2]] <- .sel_covs(y - plogis(current_prediction), x, cov_types,
+                               n_covs, method=sel_method, invmat=invmat)
 
   # Fit first non-constant model
   model[[2]][[1]] <- copulareg::copulareg(
@@ -59,21 +47,20 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
     extra_x = extra_x[, model[[2]][[2]]]
     )
 
-  time <- proc.time()
+
   curr_incr <- copulareg::predict.copulareg(model[[2]][[1]])
   if (ml_update){
-    scaling[1] <- optim(par=1,
+    scaling[2] <- optim(par=1,
                         fn=function(theta) -ll(current_prediction +
                                                  theta*curr_incr, y=y),
-                        method = "L-BFGS-B", lower = 0, upper = 100
+                        method = "Brent", lower = 0, upper = 100
                         )$par
   }
 
-  current_prediction <- current_prediction + learning_rate*scaling[1]*curr_incr
+  current_prediction <- current_prediction + learning_rate*scaling[2]*curr_incr
 
-  if (verbose){
-    cat(paste0("First integration took ", round(proc.time()[3] - time[3], 4),
-               " seconds.\n"))
+  if (verbose) {
+    setTxtProgressBar(pb, value = 2)
   }
 
   # Fit the rest of the models
@@ -81,13 +68,8 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
 
     for (m in 3:(n_models + 1)){
 
-      # Choose covariates to be included in the model
-      # random sampling as a placeholder for now
-      #model[[m]][[2]] <- sample(dim(x)[2], n_covs)
-      p_val <- compute_p_value_weights(y-plogis(current_prediction),
-                                       x, cov_types)
-      model[[m]][[2]] <- sample(dim(x)[2], n_covs, prob = 1 - p_val)
-
+      model[[m]][[2]] <- .sel_covs(y - plogis(current_prediction), x, cov_types,
+                                   n_covs, method=sel_method, invmat=invmat)
       # Fit model
       if(subsample_rows){
 
@@ -112,7 +94,6 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
           )
       }
 
-      time <- proc.time()
       if (subsample_rows){
         curr_incr <- copulareg::predict.copulareg(
           model[[m]][[1]],
@@ -126,18 +107,17 @@ copulaboost <- function(y, x, cov_types, n_models = 2, n_covs=5,
           )
       }
       if (ml_update) {
-        scaling[m-1] <- optim(par=1,
-                              fn=function(theta) -ll(current_prediction +
-                                                       theta*curr_incr, y=y),
-                              method = "L-BFGS-B", lower = 0, upper = 100
-                              )$par
+        scaling[m] <- optim(par=1,
+                            fn=function(theta) -ll(current_prediction +
+                                                     theta*curr_incr, y=y),
+                            method = "Brent", lower = 0, upper = 100
+        )$par
 
       }
       current_prediction <- (current_prediction +
-                               learning_rate*scaling[m-1]*curr_incr)
-      if (verbose){
-        cat(paste0("Integration no. ", m-1, " took ",
-                   round(proc.time()[3] - time[3], 4), " seconds.\n"))
+                               learning_rate*scaling[m]*curr_incr)
+      if (verbose) {
+        setTxtProgressBar(pb, value = m)
       }
     }
   }
@@ -205,5 +185,134 @@ predict.copulaboost <- function(model, new_x=NULL, eps=NULL,
   }
 
   list(individual=prediction, total = t(apply(prediction, 1, cumsum)))
+
+}
+
+# Covariate selection methods
+.sel_covs <- function(y, x, x_type, n_covs, method="p-value", invmat=NULL){
+
+  if (method == "p-value"){
+    .p_val_sel(y, x, x_type, n_covs)
+  } else if (method == "p-value-ridge") {
+    .ridge_p_val_sel(y, x, x_type, n_covs, invmat = invmat)
+    } else if (method == "cor-greedy") {
+    .greedy_cor_sel(y, x, n_covs)
+    } else if (method == "random") {
+    sample(ncol(x), n_covs)
+    } else {
+    stop(paste0("Method '", method, "' not implemented!"))
+  }
+
+}
+
+.p_val_sel <- function(y, x, x_type, n_covs) {
+
+  # This piece of code compiles the p-values of the regression coefficients
+  # of a univariate linear fit, for each continuous and binary variable,
+  # and the minimum p-value of the regression coefficients of categorical
+  # variables.
+
+  compute_p_value_weights <- function(y, x, x_type){
+
+    x <- as.data.frame(x)
+    vals <- rep(1, ncol(x))
+    vals[x_type == "d"] <- apply(x[, x_type=="d"], 2,
+                                 function(x) length(unique(x)) - 1)
+    colind <- cumsum(vals)
+    x[, which(x_type=="d")] <- apply(x[, which(x_type == "d")], 2, as.factor)
+    x[, which(x_type=="c")] <- apply(x[, which(x_type == "c")], 2, as.numeric)
+
+    x_ <- model.matrix(~., data=as.data.frame(x))
+    lmod <- lm(y~x_-1)
+    t_vals <- summary(lmod)$coef[-1, 4]
+
+    c(min(t_vals[1:colind[1]]),
+      sapply(2:length(colind),
+             function(j) min(t_vals[(colind[j-1] + 1):(colind[j])])))
+
+  }
+
+  p_val <- compute_p_value_weights(y, x, x_type)
+
+  sample(dim(x)[2], n_covs, prob = 1 - p_val)
+
+}
+
+.comp_invmat_ridge <- function(x, x_type, lambda=1){
+
+  x <- as.data.frame(x)
+  vals <- rep(1, ncol(x))
+  vals[x_type == "d"] <- apply(x[, x_type=="d"], 2,
+                               function(x) length(unique(x)) - 1)
+  colind <- cumsum(vals)
+  x[, which(x_type=="d")] <- apply(x[, which(x_type == "d")], 2, as.factor)
+  x[, which(x_type=="c")] <- apply(x[, which(x_type == "c")], 2, as.numeric)
+
+  X <- model.matrix(~., data=as.data.frame(x))
+
+  # First compute inverse of (X^tX + lambdaI), which should be provided, really.
+  invmat <- solve(t(X)%*%X + diag(rep(lambda, ncol(X))))
+
+}
+
+.ridge_p_val_sel <- function(y, x, x_type, n_covs, lambda = 1, invmat = NULL){
+
+  x <- as.data.frame(x)
+  vals <- rep(1, ncol(x))
+  vals[x_type == "d"] <- apply(x[, x_type=="d"], 2,
+                               function(x) length(unique(x)) - 1)
+  colind <- cumsum(vals)
+  x[, which(x_type=="d")] <- apply(x[, which(x_type == "d")], 2, as.factor)
+  x[, which(x_type=="c")] <- apply(x[, which(x_type == "c")], 2, as.numeric)
+
+  X <- model.matrix(~., data=as.data.frame(x))
+
+  # First compute inverse of (X^tX + lambdaI), which should be provided, really.
+  if(is.null(invmat)){
+    invmat <- .comp_invmat_ridge(x, xtype, lambda=lambda)
+  }
+
+  hat <- X %*% invmat %*% t(X)
+  nu <- nrow(X) - sum(diag(2*hat - hat %*% t(hat)))
+  betahat <- invmat %*% t(X) %*% y
+  sigmahat <- as.numeric((t(y - X %*% betahat) %*% (y - X %*% betahat))/nu)
+  var_betahat <- diag(sigmahat * (invmat %*% (t(X) %*% X) %*% invmat))
+
+  vals <- 2*pt(-abs(betahat / sqrt(var_betahat)), df=nu)
+
+  p_fin <- c(min(vals[1:colind[1]]),
+             sapply(2:length(colind),
+                    function(j) min(vals[(colind[j-1] + 1):(colind[j])])))
+
+
+  sample(dim(x)[2], n_covs, prob = 1 - p_fin)
+}
+
+
+
+.greedy_cor_sel <- function(y, x, n_covs, start="random"){
+
+  # Initital covariate
+  if (start=="random"){
+    k <- sample(ncol(x), 1)
+  } else if (start == "maxcor"){
+    k <- which.max(abs(cor(x, y, method="kendall")))
+  } else {
+    stop(paste0("Start: ", start, " not implemented."))
+  }
+
+  for (j in 2:n_covs){
+    add_2_p1 <- sapply((1:ncol(x))[-k], function(i) abs(cor(x[, i], y,
+                                                            method = "kendall")))
+    add_2_p2 <- sapply((1:ncol(x))[-k],
+                       function(i) sum(abs(cor(x[, k], x[, i],
+                                               method = "kendall"))))
+    cov2add <- which.max((add_2_p1 + obj_p1) / (obj_p2 + add_2_p2))
+    k <- c(k, (1:ncol(x))[-k][cov2add])
+    obj_p1 <- obj_p1 + add_2_p1[k]
+    obj_p2 <- obj_p2 + add_2_p2[k]
+  }
+
+  return(k)
 
 }
